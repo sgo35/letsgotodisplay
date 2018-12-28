859d845143e778ca8e14822465cb8b562ace5106
sgo35 <jobsgoud@hotmail.fr>
2018-12-28 18:10:42 +0100

    Autocomplet cities

--- src/main/java/com/letsgo/todisplay/LetsgoDisplayApplication.java
+++ src/main/java/com/letsgo/todisplay/LetsgoDisplayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import com.letsgo.todisplay.model.DataLayout;
 import com.letsgo.todisplay.model.LayoutTpl;
